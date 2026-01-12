bd2b3bf9387b9eccfe471f3a1ce774e1a0b949b0
Vix <vixnf05@gmail.com>
2026-01-12 17:00:05 +0800

    Initial commit

---
+++ src/main/java/net/vix/gacow/GAcow.java
@@ -0,0 +1,17 @@
+package net.vix.gacow;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GAcow implements ModInitializer {
+	public static final String MOD_ID = "gacow";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/vix/gacow/GAcowClient.java
@@ -0,0 +1,10 @@
+package net.vix.gacow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GAcowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
