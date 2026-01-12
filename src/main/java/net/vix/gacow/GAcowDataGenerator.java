30dcd9bd88e921e814a19b1de57719d680250552
Vix <vixnf05@gmail.com>
2026-01-12 17:02:03 +0800

    added a test comment!

--- src/main/java/net/vix/gacow/GAcowDataGenerator.java
+++ src/main/java/net/vix/gacow/GAcowDataGenerator.java
@@ -2,7 +2,7 @@ package net.vix.gacow;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-
+//test comment!
 public class GAcowDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
